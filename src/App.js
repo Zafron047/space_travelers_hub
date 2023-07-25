99a409a7f78f9cc0f9dfe3977c737684115af8fa
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-07-25 22:26:24 +0600

    Dispatch API fetching functionality to be default on page load

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Nav from './components/Nav';
+import { fetchRockets } from './redux/rockets/rocketSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
     <div className="main">
       <Nav />
       <Routes>
-        <Route className="navbar-link" path="/rockets" />
+        <Route className="navbar-link" path="/" />
         <Route className="navbar-link" path="/missions" />
         <Route className="navbar-link" path="/profile" />
       </Routes>
