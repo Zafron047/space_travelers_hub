6fe6195bff15535ec9c9e70d1ce284d9f8028ecb
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-07-27 23:05:14 +0600

    Add test for Rocket.js

---
+++ src/__tests__/Rockets.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Rockets from '../components/Rockets';
+import store from '../redux/store';
+const render = (component) => rtlRender(
+  <Provider store={store}>
+    {component}
+  </Provider>,
+);
+describe('MyRocketss', () => {
+  it('render Rockets Components', () => {
+    render(<Rockets />);
+    expect(Rockets).toMatchSnapshot();
+  });
+});
